2732d4b6e1018ba1776fdfed17745b6c63a1497f
apfelstrudelweb <ulrich.vormbrock@gmail.com>
2016-06-28 12:10:33 +0200

    Additional 6-string guitar tunings

--- Simple Ukulele Tuner/Guitar_Constants.h
+++ Simple Ukulele Tuner/Guitar_Constants.h
@@ -11,23 +11,27 @@
 
 
 #define APPSTORE_ID @"1128226635"
-#define STANDARD_SUBTYPE             @[@0, @5] // Acoustic Standard (E - A - D - G - B - E) and Bass Standard (E - A - D -G)
+#define STANDARD_SUBTYPE             @[@0, @8] // Acoustic Standard (E - A - D - G - B - E) and Bass Standard (E - A - D -G)
 
 
 #define KEY_GUITAR_TYPE          @"guitarType"
+// 6 String Acoustic
 #define GUITAR_TYPE_01           @"6 - Classical / Acoustic (E - A - D - G - B - E)"
+#define GUITAR_TYPE_02           @"6 - Classical Drop D (D - A - D - G - B - E)"
+#define GUITAR_TYPE_03           @"6 - Classical Drop C (C - G - C - F - A - D)"
+#define GUITAR_TYPE_04           @"6 - Classical Open G (D - G - D - G - B - D)"
 // 5 String Bass
-#define GUITAR_TYPE_02           @"5 - Bass Standard (B - E - A - D - G)"
-#define GUITAR_TYPE_03           @"5 - Bass High C (E - A - D - G - C)"
-#define GUITAR_TYPE_04           @"5 - Bass Half Step Down (A# - D# - G# - F - F#)"
-#define GUITAR_TYPE_05           @"5 - Bass Full Step Down (A - D - G - C - F)"
+#define GUITAR_TYPE_05           @"5 - Bass Standard (B - E - A - D - G)"
+#define GUITAR_TYPE_06           @"5 - Bass High C (E - A - D - G - C)"
+#define GUITAR_TYPE_07           @"5 - Bass Half Step Down (A# - D# - G# - F - F#)"
+#define GUITAR_TYPE_08           @"5 - Bass Full Step Down (A - D - G - C - F)"
 // 4 String Bass
-#define GUITAR_TYPE_06           @"4 - Bass Standard (E - A - D - G)"
-#define GUITAR_TYPE_07           @"4 - Bass Drop D (D - A - D - G)"
-#define GUITAR_TYPE_08           @"4 - Bass Half Step Down (D# - G# - C# - A#)"
-#define GUITAR_TYPE_09           @"4 - Bass Full Step Down (D - G - C - A)"
-#define GUITAR_TYPE_10           @"4 - Bass Low B (B - E - A - D)"
-#define GUITAR_TYPE_11           @"4 - Bass Drop C (C - A - D - G)"
+#define GUITAR_TYPE_09           @"4 - Bass Standard (E - A - D - G)"
+#define GUITAR_TYPE_10           @"4 - Bass Drop D (D - A - D - G)"
+#define GUITAR_TYPE_11           @"4 - Bass Half Step Down (D# - G# - C# - A#)"
+#define GUITAR_TYPE_12           @"4 - Bass Full Step Down (D - G - C - A)"
+#define GUITAR_TYPE_13           @"4 - Bass Low B (B - E - A - D)"
+#define GUITAR_TYPE_14           @"4 - Bass Drop C (C - A - D - G)"
 
 
 
